47e2b37cfa78c4b4b16d47f3de117d61cf933d36
aleleonel <aleleonel@gmail.com>
2019-12-16 16:18:50 -0300

    autenticações de login e logout

--- agenda/urls.py
+++ agenda/urls.py
@@ -14,12 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from core import views
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('agenda/', views.lista_eventos),
     path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('agenda/', views.lista_eventos),
+    path('logout/', views.logout_user),
+
+
+
 ]
